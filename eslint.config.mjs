77e0d1c77acdd9acd841c30394012e83a4b43f2e
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-06-03 17:09:30 +0530

    Set up testing with jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginJest from 'eslint-plugin-jest';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  eslintPluginJest,
 ];
